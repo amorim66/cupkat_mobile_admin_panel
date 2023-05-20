3d2d515a20af51bf6b7d7df3638d835b77bba22f
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-05-19 23:08:38 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cupkat_multi_web_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cupkat_multi_web_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:cupkat_multi_web_admin/views/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: kIsWeb || Platform.isAndroid
+          ? FirebaseOptions(
+              apiKey: "AIzaSyA4q_689yJDIUHKybZzhK7Ho1-nWj8JhRU",
+              appId: "1:280940830966:web:7b6fdc06f0cc5c6e038cd7",
+              messagingSenderId: "280940830966",
+              projectId: "cupkat-mobile-multi-store",
+              storageBucket: "cupkat-mobile-multi-store.appspot.com",
+            )
+          : null);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+      builder: EasyLoading.init(),
+    );
+  }
+}
